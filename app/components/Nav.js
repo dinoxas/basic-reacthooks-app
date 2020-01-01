e420c39080299079f8c438885a3cea406ed4e5ac
dinoxas <dhassiotis07@gmail.com>
2020-01-01 18:23:33 +0000

    Refactored theme state to use useState Hook instead and separated toggleTheme as a prop

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,43 +1,46 @@
-import React from 'react'
-import { ThemeConsumer } from '../contexts/theme'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { ThemeConsumer } from "../contexts/theme";
+import { NavLink } from "react-router-dom";
 
 const activeStyle = {
-  color: 'rgb(187, 46, 31)'
-}
+  color: "rgb(187, 46, 31)"
+};
 
-export default function Nav () {
+export default function Nav({ toggleTheme }) {
   return (
     <ThemeConsumer>
-      {({ theme, toggleTheme }) => (
-        <nav className='row space-between'>
-          <ul className='row nav'>
+      {theme => (
+        <nav className="row space-between">
+          <ul className="row nav">
             <li>
-              <NavLink 
-              exact
-              activeStyle={activeStyle}
-              to='/' 
-              className='nav-link'>
+              <NavLink
+                exact
+                activeStyle={activeStyle}
+                to="/"
+                className="nav-link"
+              >
                 Popular
               </NavLink>
             </li>
             <li>
-              <NavLink 
-              activeStyle={activeStyle}
-              to='/battle' 
-              className='nav-link'>
-                Battle</NavLink>
+              <NavLink
+                activeStyle={activeStyle}
+                to="/battle"
+                className="nav-link"
+              >
+                Battle
+              </NavLink>
             </li>
           </ul>
           <button
-            style={{fontSize: 30}}
-            className='btn-clear'
+            style={{ fontSize: 30 }}
+            className="btn-clear"
             onClick={toggleTheme}
           >
-            {theme === 'light' ? '🔦' : '💡'}
+            {theme === "light" ? "🔦" : "💡"}
           </button>
         </nav>
       )}
     </ThemeConsumer>
-  )
+  );
 }
